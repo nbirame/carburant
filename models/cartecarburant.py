3ade8710c7fca27309277c2f6dfbd49ab0020dec
nbirame <biramendiaye71@gmail.com>
2025-02-19 16:18:18 +0000

    correction code

--- models/cartecarburant.py
+++ models/cartecarburant.py
@@ -34,7 +34,7 @@ class Carte(models.Model):
     # methode qui permet de calculer le nombre de littre consommer
     @api.onchange("consommation_ids")
     def _onchange_nb_littre(self):
-        carburant_consommer = 0
+        # carburant_consommer = 0
         carte = self.env["carburant.cartecarburant"].sudo().search([])
         for carburant in carte:
             carburant_consommer = carburant.nb_littre
